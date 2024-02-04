e1e298018eb2ef0b847a207c62a5ec4a73ffe3b2
33-66 <simon.nganga@student.moringaschool.com>
2024-02-04 09:53:27 +0300

    Added get by ID route

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+class BirdByID(Resource):
+    def get(self, id):
+        bird = Bird.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(bird), 200)
+
+api.add_resource(BirdByID, '/birds/<int:id>')
 
 
 
